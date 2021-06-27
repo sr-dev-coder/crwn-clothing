f38958cee8f823d674fd5a6de184dc0a6a0d1452
Sandeep Rohilla <makaankhareedo.srohilla@gmail.com>
2021-06-27 18:51:32 +0530

    broken homepage in small component

---
+++ src/component/menu-item/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+export const MenuItem = ({ title, imageUrl, size }) =>{
+    return (
+        <div className={`${size} menu-item`}>
+          <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`  }} />
+          <div className='content'>
+            <div className='title'>{title}</div>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+    )
+}
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Directory from './../../component/directory/directory.component';
+import './homepage.styles.scss';
+
+export const HomePage = () => (
+    <div className='homepage'>
+      <h1>Welcome to my Homepage</h1>
+      <Directory />
+     </div>
+  );
