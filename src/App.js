ce400052bf168ce03b3039f4ddead46e1f2dddd6
Sandeep Rohilla <makaankhareedo.srohilla@gmail.com>
2021-07-11 13:14:58 +0530

    implementd firebase utils, include ability to authinticate user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { auth } from './firebase/firebase.utils';
 import { Switch, Route  } from 'react-router-dom';
 import './App.scss';
 import Header from './component/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-signup-page/sign-in-and-signup-page.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -19,8 +19,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/sign-in-and-signup-page/sign-in-and-signup-page.component.jsx
+++ src/pages/sign-in-and-signup-page/sign-in-and-signup-page.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './sign-in-and-signup-page.styles.scss';
 
 import SignIn from './../../component/signin/signin.component';
+import SignUp from './../../component/sign-up/sign-up.component';
 
 const SignInAndSignUp = () =>(
     <div className='sign-in-and-signup'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
