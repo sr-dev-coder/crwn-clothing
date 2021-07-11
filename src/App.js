02f2c064a6fdf17cbe93e29c6e18dbcdbca5ccd4
Sandeep Rohilla <makaankhareedo.srohilla@gmail.com>
2021-07-11 13:32:24 +0530

    finishing sign in witheamil and password

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
